9c44405443a9e24dbc6c9e74b8138183268b8d61
sleeplesseditor <josh@bryson.me.uk>
2018-12-07 16:48:13 +0000

    OpenWeather API setup

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, StyleSheet, Text, View } from 'react-native';
+
+import { API_KEY } from './utils/weatherAPIKey';
+import Weather from './components/Weather';
 
 export default class App extends React.Component {
+  state = {
+    isLoading: false,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
+
   render() {
+    const { isLoading } = this.state;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <View style={styles.container}>
+          {isLoading ? (
+            <Text>Fetching the Weather</Text>
+          ) : (
+            <View>
+              <Text>Minimalist Weather App</Text>
+            </View>
+          )}
+        </View>
     );
   }
 }
